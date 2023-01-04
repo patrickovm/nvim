70f7fd85a3de2c6ba9b84774a2fe6818a866c581
Patrick Monteiro <patrickovm.eng@gmail.com>
2023-01-03 21:45:49 -0300

    Add alpha greeter

--- lua/patrick/plugins.lua
+++ lua/patrick/plugins.lua
@@ -74,6 +74,9 @@ return packer.startup(function(use)
   -- Git
 	use({ "lewis6991/gitsigns.nvim", commit = "f98c85e7c3d65a51f45863a34feb4849c82f240f" })
 	use({ "f-person/git-blame.nvim", commit = "08e75b7061f4a654ef62b0cac43a9015c87744a2" })
+
+  -- Startup
+	use({ "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" })
 	
   -- Icons
 	use({ "kyazdani42/nvim-web-devicons", commit = "563f3635c2d8a7be7933b9e547f7c178ba0d4352" })
