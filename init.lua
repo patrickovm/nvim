daf75058b9643606e20bad82d333808808b98f71
Patrick Monteiro <patrickovm.eng@gmail.com>
2026-05-11 15:22:20 -0300

    add luacheckrc and vscode handling

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+if vim.g.vscode then
+    vim.opt.clipboard = "unnamedplus"
+else
+    vim.g.mapleader = " "
+    vim.g.maplocalleader = " "
 
-require("config")
-require("plugins")
+    require("config")
+    require("plugins")
+end
